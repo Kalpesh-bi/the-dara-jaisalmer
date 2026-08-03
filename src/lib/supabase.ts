4ee4f45995bc0513c7d444dcc77b4288c20cf18e
Kalpesh-bi <kalpeshsuthar.pbs@email.com>
2026-08-03 12:39:55 +0530

    Fix Supabase configuration

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,11 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+console.log(import.meta.env);
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+console.log("URL:", supabaseUrl);
+console.log("KEY:", !!supabaseAnonKey);
+
+export const supabase = createClient(supabaseUrl!, supabaseAnonKey!);
